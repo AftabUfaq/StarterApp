b322a49ccadc99a8bb3d1c2e4dab6a83ac3e9906
aftabameen <aftabameen@recruitbpm.com>
2022-06-28 12:14:20 +0500

    Swipe BUtton Added

--- src/screens/OnBoardingScreen/index.js
+++ src/screens/OnBoardingScreen/index.js
@@ -5,6 +5,7 @@ import { commonStyles,textStyles } from '../../styles';
 import CustomHeader from '../../components/CustomHeader';
 import { colors, fonts } from '../../constants/theme';
 import { AppScreenWidth, hp, width } from '../../constants/sacling';
+import Swiptbutton from './SwipeButton';
     const OnBoardingScreen = ({navigation}) => {
         return (
             <SafeAreaView style={{flex:1, backgroundColor:colors.dark_primary_color}} >
@@ -16,6 +17,23 @@ import { AppScreenWidth, hp, width } from '../../constants/sacling';
                         onPress={() => navigation.goBack()}
                         title={"OnBoarding"}
                     />
+                    <Swiptbutton 
+                        title={"Button 1"}
+                        onPress={() => alert("Button 1")}
+                    />
+                    <Swiptbutton 
+                        title={"Button 2"}
+                        onPress={() => alert("Button 2")}
+                    />
+                        <Swiptbutton 
+                        title={"Button 3"}
+                        onPress={() => alert("Button 3")}
+                    />
+                        <Swiptbutton 
+                        title={"Button 4"}
+                        onPress={() => alert("Button 4")}
+                    />
+
                 </View>
             </SafeAreaView>
             
